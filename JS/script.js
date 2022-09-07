2e3ee3412111774cbf3b3ef0ca8916197f0c0efe
BalaguerDev <101297683+BalaguerDev@users.noreply.github.com>
2022-09-07 06:35:39 +0200

    a

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
-$("#toggle").click(function() {
-    $(this).toggleClass("on");
-    $("#menu").slideToggle();
-  });
+const hamburger =document.querySelector(".hamburger");
+const navMenu =document.querySelector(".navMenu");
+
+hamburger.addEventListener("click",() => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".navLink").forEach(i =>i.addEventListener("click", ()=> {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
---
+++ html/script2.js
@@ -0,0 +1,15 @@
+
+/* TOGGLE NAVMENU RESPONSIVE */
+const hamburger = document.querySelector (".hamburger");
+const navMenu = document.querySelector (".navMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".navLink").forEach(i => i.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
